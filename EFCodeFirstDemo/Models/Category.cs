e4b07a0a52841dac57fe4aba019ae9bf4c99ce6a
Shon <hsuizip@gmail.com>
2018-07-31 14:09:15 +0800

    建立Model

---
+++ EFCodeFirstDemo/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace EFCodeFirstDemo.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.ProductList = new HashSet<Product>();
+        }
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> ProductList{ get; set; }
+    }
+}
