2c6fb826624b773cc44e0d82fe67836c6a11a184
Shon <hsuizip@gmail.com>
2018-07-31 14:20:28 +0800

    建立DbContext

---
+++ EFCodeFirstDemo/Models/MyDbcontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirstDemo.Models
+{
+    public class MyDbcontext : DbContext
+    {
+        public MyDbcontext()
+            : base("name=MyDbcontext")
+        {
+
+        }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+    }
+}
