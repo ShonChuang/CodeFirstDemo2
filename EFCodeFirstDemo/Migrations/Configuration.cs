84f2ca5771307133f725711527ac453c411230d3
Shon <hsuizip@gmail.com>
2018-07-31 14:31:05 +0800

    設定種子資料

--- EFCodeFirstDemo/Migrations/Configuration.cs
+++ EFCodeFirstDemo/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace EFCodeFirstDemo.Migrations
 {
+    using EFCodeFirstDemo.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,6 +16,22 @@ namespace EFCodeFirstDemo.Migrations
 
         protected override void Seed(EFCodeFirstDemo.Models.MyDbcontext context)
         {
+            var initialCatgory = new List<Category>()
+            {
+                new Category{ CategoryName ="c1" },
+                new Category{ CategoryName ="c2" },
+                new Category{ CategoryName ="c3" }
+            };
+            initialCatgory.ForEach(d => context.Categories.AddOrUpdate(s => s.CategoryName, d));
+            context.SaveChanges();
+
+            var InitialProduct = new List<Product>() {
+                new Product { ProductName="p1",CategoryId=1 },
+                new Product { ProductName="p2",CategoryId=2 },
+                new Product { ProductName="p3",CategoryId=1 }
+            };
+            InitialProduct.ForEach(p => context.Products.AddOrUpdate(s => s.ProductName, p));
+            context.SaveChanges();
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
